5970a60ee7660bcafb6681e0709a79105fcba1f5
Alex-V-Kovalev <a.kovalev@mail.ru>
2021-02-10 20:02:06 +0300

    MainApp: добавил 3 варианта действия

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -1,34 +1,74 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
+import hiber.service.UserServiceImp;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Scanner;
 
 public class MainApp {
-   public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
+    public static void main(String[] args) throws SQLException {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Что делаем?");
+        System.out.println("===============================");
+        System.out.println("1: добавляем 4 дефолтных юзера");
+        System.out.println("2: выводим всех юзеров");
+        System.out.println("3: посмотрим у кого таз");
+        System.out.print("> ");
+        String choice = scanner.nextLine();
 
-      UserService userService = context.getBean(UserService.class);
+        UserService userService = context.getBean(UserService.class);
+        switch (choice) {
+            case "1":
+                Car car = new Car("Lambo", 111);
+                User user = new User("User1", "Lastname1", "user1@mail.ru", car);
+                userService.add(user);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+                car = new Car("Mitsu", 222);
+                user = new User("User2", "Lastname2", "user2@mail.ru", car);
+                userService.add(user);
 
-      List<User> users = userService.listUsers();
-      for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println();
-      }
+                car = new Car("VAZ", 99999);
+                user = new User("User3", "Lastname3", "user3@mail.ru", car);
+                userService.add(user);
 
-      context.close();
-   }
+                car = new Car("Mustang", 123);
+                user = new User("User4", "Lastname4", "user4@mail.ru", car);
+                userService.add(user);
+                break;
+            case "2":
+                List<User> users = userService.listUsers();
+                for (User usr : users) {
+                    System.out.println("Id = " + usr.getId());
+                    System.out.println("First Name = " + usr.getFirstName());
+                    System.out.println("Last Name = " + usr.getLastName());
+                    System.out.println("Email = " + usr.getEmail());
+                    System.out.println("============================");
+                    System.out.println("Car = " + usr.getCar());
+                    System.out.println();
+                }
+                break;
+            case "3":
+                User oneUser = userService.getUserByCarModelAndSeries("VAZ", 99999);
+                System.out.println("Id = " + oneUser.getId());
+                System.out.println("First Name = " + oneUser.getFirstName());
+                System.out.println("Last Name = " + oneUser.getLastName());
+                System.out.println("Email = " + oneUser.getEmail());
+                System.out.println("============================");
+                System.out.println("Car = " + oneUser.getCar());
+                System.out.println();
+                break;
+            default:
+                System.out.println("Чет не то выбрал, перезапусти программу и убедись что выбераеш 1, 2 или 3");
+                System.out.println("bye-bye");
+        }
+        context.close();
+    }
 }
