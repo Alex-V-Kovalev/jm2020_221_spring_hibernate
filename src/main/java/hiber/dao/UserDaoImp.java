b815d7059c217fd34966635c07912e5dbe8ce763
Alex-V-Kovalev <a.kovalev@mail.ru>
2021-02-10 19:39:28 +0300

    UserDao: добавлено описание метода для получения user по модели и серии car UserDaoImp: реализован метод из интерфейса для получения user по модели и серии car UserService: добавлен аналогичный UserDao метод для получения user по модели и серии car UserServiceImp: добавлена реализация метода для получения user по модели и серии car

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -1,6 +1,7 @@
 package hiber.dao;
 
 import hiber.model.User;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -11,19 +12,28 @@ import java.util.List;
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+    @Autowired
+    private SessionFactory sessionFactory;
 
-   @Override
-   public void add(User user) {
-      sessionFactory.getCurrentSession().save(user);
-   }
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().save(user);
+    }
 
-   @Override
-   @SuppressWarnings("unchecked")
-   public List<User> listUsers() {
-      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
-      return query.getResultList();
-   }
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
+
+    @Override
+    public User getUserByCarModelAndSeries(String model, int series) {
+        TypedQuery<User> query = sessionFactory.getCurrentSession()
+                .createQuery("FROM User usr WHERE usr.car.model = :car_model AND usr.car.series = :car_series", User.class)
+                .setParameter("car_model", model)
+                .setParameter("car_series", series);
+        return query.getSingleResult();
+    }
 
 }
